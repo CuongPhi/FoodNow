a899fde381c3a2edeb47d9ac5d0fe9d80f770322
annguyen011197 <tranminhannguyen@gmail.com>
2018-09-23 02:07:45 +0700

    +ForgotPasswordScreen

--- src/FD_1512020/App.js
+++ src/FD_1512020/App.js
@@ -8,41 +8,16 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import SignInScreen from './src/cpn/SignIn/SignInScreen'
+import SignUpScreen from './src/cpn/SignUp/SignUpScreen'
+import ForgotPasswordScreen from './src/cpn/ForgotPassword/ForgotPasswordScreen'
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ForgotPasswordScreen/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
---
+++ src/FD_1512020/src/cpn/ForgotPassword/ForgotPasswordScreen.js
@@ -0,0 +1,56 @@
+import React, { PureComponent } from 'react'
+import {
+    View,
+    ImageBackground,
+    StyleSheet,
+    Image,
+} from 'react-native'
+import ForgotPassword from './ForgotPassword'
+
+//Should merge with SiginScreen
+export default class ForgotPasswordScreen extends PureComponent {
+    render() {
+        const { main, brand, brandWrapper, inputLayout, input } = styles
+        return (
+            <ImageBackground
+                source={require('../../assets/image/FD_background.jpg')}
+                style={main}
+                blurRadius={0.5}
+            >
+                <View
+                    style={brandWrapper}
+                >
+                    <Image
+                        source={require('../../assets/image/logo.png')}
+                        style={brand}
+                    />
+                </View>
+                <ForgotPassword style={{flex:1}}/>
+            </ImageBackground>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    brand: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'contain',
+    },
+    brandWrapper: {
+        flex: 1,
+        width: '80%',
+    },
+    inputLayout: {
+        marginVertical: 20, width: '80%'
+    },
+    input: {
+        height: 50
+    }
+})
