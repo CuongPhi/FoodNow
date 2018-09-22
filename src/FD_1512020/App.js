0d8ddedacce7352b86919d549841846a134cfec3
annguyen011197 <tranminhannguyen@gmail.com>
2018-09-23 03:59:41 +0700

    +MerchantListScreen +react-native-content-loader +react-native-svg

--- src/FD_1512020/App.js
+++ src/FD_1512020/App.js
@@ -6,16 +6,31 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { ScrollView, View, Dimensions } from 'react-native';
 import SignInScreen from './src/cpn/SignIn/SignInScreen'
 import SignUpScreen from './src/cpn/SignUp/SignUpScreen'
 import ForgotPasswordScreen from './src/cpn/ForgotPassword/ForgotPasswordScreen'
-
+import MerchanListScreen from './src/cpn/MerchantList/MerchanListScreen'
 export default class App extends Component {
+
   render() {
+    var { height, width } = Dimensions.get('window');
+
     return (
-      <ForgotPasswordScreen/>
+      <MerchanListScreen/>
+      // <ScrollView
+      // >
+      //   <View style={{height:height}}>
+      //     <SignInScreen />
+      //   </View>
+      //   <View style={{height:height}}>
+      //     <SignUpScreen />
+      //   </View>
+      //   <View style={{height:height}}>
+      //     <ForgotPasswordScreen />
+      //   </View>
+      // </ScrollView>
     );
   }
 }
