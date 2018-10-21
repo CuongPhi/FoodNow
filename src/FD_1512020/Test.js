c81d4e597d452afe5e4fafef24240a1af74cac86
annguyen011197 <tranminhannguyen@gmail.com>
2018-10-21 23:28:05 +0700

    Release v0.04

---
+++ src/FD_1512020/Test.js
@@ -0,0 +1,10 @@
+// function isEmptyOrNull(text) {
+//   if(typeof text ==)
+//   return String(text).length === 0;
+// }
+
+// console.log(isEmptyOrNull(undefined));
+// console.log(isEmptyOrNull(''));
+// console.log(isEmptyOrNull(2323));
+// console.log(isEmptyOrNull('3232323'));
+// console.log(isEmptyOrNull(true));
